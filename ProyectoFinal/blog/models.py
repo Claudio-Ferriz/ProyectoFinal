5b8f368af1fa6bcab01cd5fd81e26ba5870c9c59
ConradoPerez <88351398+ConradoPerez@users.noreply.github.com>
2022-10-12 13:17:01 -0300

    agrego detalles

--- ProyectoFinal/blog/models.py
+++ ProyectoFinal/blog/models.py
@@ -1,11 +1,10 @@
-
+from pyexpat import model
 from django.db import models
-
-# Create your models here.
 
 class Autor(models.Model):
     class Meta: 
         verbose_name_plural = "Autores"
+        
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     profesion = models.CharField(max_length=30) 
@@ -19,9 +18,16 @@ class Articulo(models.Model):
     titulo = models.CharField(max_length=50)
     texto = models.CharField(max_length=1000)
     fecha = models.DateField(null = True)
+    
+    def __str__(self):
+        return self.titulo
 
 
 class Seccion(models.Model):
     class Meta: 
         verbose_name_plural = "Secciones"
+        
     nombre = models.CharField(max_length=50)   
+    
+    def __str__(self):
+        return self.nombre
