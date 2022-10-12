40051825c7dbc6a2f2c70f37c30620a6b81850d8
ConradoPerez <88351398+ConradoPerez@users.noreply.github.com>
2022-10-12 11:46:28 -0300

    Add files via upload

--- ProyectoFinal/blog/views.py
+++ ProyectoFinal/blog/views.py
@@ -11,15 +11,18 @@ def mostrar_inicio(request):
     return HttpResponse("Bienvenidos al inicio")
 
 def procesar_formulario_autor(request):
-    contexto = {}
+    mi_formulario = AutorForm()
+    contexto = {"formulario": mi_formulario}
     return render(request,"blog/formulario-autor.html", context=contexto)
 
 def procesar_formulario_articulo(request):
-    contexto = {}
+    mi_formulario = ArticuloForm()
+    contexto = {"formulario": mi_formulario}
     return render(request,"blog/formulario-articulo.html",context=contexto)
 
 def procesar_formulario_seccion(request):
-    contexto = {}
+    mi_formulario = SeccionForm()
+    contexto = {"formulario": mi_formulario}
     return render(request,"blog/formulario-seccion.html",context=contexto)
 
 def buscar(request):
